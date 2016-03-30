d437564defa598009b4684781d58799d30fdccb8
Artur Latypov <tinypony@github.com>
2016-03-30 13:01:52 +0300

    adde upstart script

--- install.sh
+++ install.sh
@@ -4,8 +4,13 @@ mkdir /opt/monitoring-configurator
 cp -r ./* /opt/monitoring-configurator/
 
 cd /opt/monitoring-configurator
-mv ./systemd/monitoring-configurator.service /etc/systemd/system/monitoring-configurator.service
-#rm -rf ./upstart
-
 npm install
-systemctl start monitoring-configurator
+
+if [[ -d "/usr/lib/systemd" ]]; then
+        echo "Use systemd"
+	mv ./systemd/monitoring-configurator.service /etc/systemd/system/monitoring-configurator.service
+        systemctl start monitoring-configurator
+elif [[ -d "/usr/share/upstart" ]]; then
+        echo "Use upstart"
+        mv ./upstart/monitoring-configurator.conf /etc/init/monitoring-configurator.conf
+fi
