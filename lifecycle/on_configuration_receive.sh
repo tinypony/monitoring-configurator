c8503b051b14e338f6fe01bde9b10749ca69568e
Artur Latypov <tinypony@github.com>
2016-06-02 12:57:34 +0300

    modify meta.properties with broker id, too, before restarting kafka

--- lifecycle/on_configuration_receive.sh
+++ lifecycle/on_configuration_receive.sh
@@ -3,6 +3,7 @@ brokid=$1
 zookeeper_host=$2
 zookeeper_port=$3
 sed -i "s/^.*broker\.id=.*$/broker.id=`echo $brokid`/g" /opt/kafka/config/server.properties
+sed -i "s/^.*broker\.id=.*$/broker.id=`echo $brokid`/g" /tmp/kafka-logs/meta.properties
 sed -i "s/^.*zookeeper\.connect=.*$/zookeeper.connect=`echo $zookeeper_host:$zookeeper_port`/g" /opt/kafka/config/server.properties
 sudo stop kafka
 sudo start kafka
