ab68c03aa34c74f7384e0e05c57b41dc2b556947
Artur Latypov <tinypony@github.com>
2016-06-02 10:34:06 +0300

    restart kafka properly in script

--- lifecycle/on_configuration_receive.sh
+++ lifecycle/on_configuration_receive.sh
@@ -4,4 +4,5 @@ zookeeper_host=$2
 zookeeper_port=$3
 sed -i "s/^.*broker\.id=.*$/broker.id=`echo $brokid`/g" /opt/kafka/config/server.properties
 sed -i "s/^.*zookeeper\.connect=.*$/zookeeper.connect=`echo $zookeeper_host:$zookeeper_port`/g" /opt/kafka/config/server.properties
-sudo restart kafka
+sudo stop kafka
+sudo start kafka
